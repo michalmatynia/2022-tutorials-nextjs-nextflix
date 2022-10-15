700d750a23a1c356c72325083a96d41cb4973d19
Michal <mmatynia@gmail.com>
2022-10-15 21:11:34 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "plugin:@next/next/recommended",
+    "prettier",
+    "eslint:recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+
+      parserOptions: {
+        project: ["./tsconfig.json"], // Specify it only for TypeScript files
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "@typescript-eslint", "react-hooks"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": ["error"],
+    "@typescript-eslint/explicit-function-return-type": "off",
+
+    "react/jsx-no-constructed-context-values": "off", //
+    "react/destructuring-assignment": "off",
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "jsx-a11y/anchor-is-valid": "off",
+
+    // 'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    // 'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    semi: ["error", "always"],
+  },
+};
