d09c9fe26262e018b728e8ed027b49d5358ef8c4
Michal <mmatynia@gmail.com>
2022-10-24 09:28:02 +0200

    ISR

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,9 +1,67 @@
 import { useRouter } from "next/router";
+import Modal from "react-modal";
+import styles from "../../styles/Video.module.css";
+
+import clsx from "classnames";
+
+Modal.setAppElement("#__next");
 
 const Video = () => {
   const router = useRouter();
 
-  return <div>Video{router.query.videoId}</div>;
+  const video = {
+    title: "Hi cute dog",
+    publishTime: "1990-01-01",
+    decription: "A big red dog is super",
+    channelTitle: "Paramount Pictures",
+    viewCount: 1000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
+
+  return (
+    <div className={styles.container}>
+      <Modal
+        isOpen={true}
+        contentLabel="Watch the Video"
+        onRequestClose={() => {
+          router.back();
+        }}
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+      >
+        <iframe
+          id="player"
+          className={styles.videoPlayer}
+          type="text/html"
+          width="100%"
+          height="390"
+          src={`http://www.youtube.com/embed/${router.query.videoId}?enablejsapi=1&origin=http://example.com&controls=0&rel=0`}
+          frameBorder="0"
+        ></iframe>
+
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default Video;
